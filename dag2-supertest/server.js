cb4a50c57e779f810ce8f8ae09e831178f8e1240
sofiaCoder <sofia.laas@gmail.com>
2023-03-28 19:02:52 +0200

    Added register with validation

--- dag2-supertest/server.js
+++ dag2-supertest/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const server = express();
+const joi = require('joi');
 server.use(express.json());
 
-server.post('/login', {
+server.post('/login', (req, res) => {
 
 })
 
-server.post('/register', {
+server.post('/register', (req, res) => {
+    const schema = joi.object({
+        username: joi.string().required(),
+        password: joi.string().required()
+    })
 
+    const { error, value } = schema.validate(req.body)
+    if(error) {
+        res.status(400).send(error.details[0].message)
+        return;
+    }
+
+    res.status(201).send('Registerd succesfully')
 })
 
-exports.module.server
+
+exports.server = server
